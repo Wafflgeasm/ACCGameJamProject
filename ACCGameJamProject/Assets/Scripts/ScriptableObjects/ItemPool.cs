147c0dcbd6e54abc8325d3fb6afcb71ec7a72a5c
Wafflgeasm <85540855+Wafflgeasm@users.noreply.github.com>
2022-10-16 18:01:22 -0500

    Fixed the namedarray attribute.

--- ACCGameJamProject/Assets/Scripts/ScriptableObjects/ItemPool.cs
+++ ACCGameJamProject/Assets/Scripts/ScriptableObjects/ItemPool.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using System;
 
 
@@ -13,6 +14,36 @@ public class NamedArrayAttribute : PropertyAttribute
     }
 }
 
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        // Properly configure height for expanded contents.
+        return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
+    }
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Replace label with enum name if possible.
+        try
+        {
+            var config = attribute as NamedArrayAttribute;
+            var enum_names = System.Enum.GetNames(config.TargetEnum);
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            var enum_label = enum_names.GetValue(pos) as string;
+            // Make names nicer to read (but won't exactly match enum definition).
+            enum_label = ObjectNames.NicifyVariableName(enum_label.ToLower());
+            label = new GUIContent(enum_label);
+        }
+        catch
+        {
+            // keep default label
+        }
+        EditorGUI.PropertyField(position, property, label, property.isExpanded);
+    }
+}
+#endif
 
 public enum eItemPool { none, oldLady, all }
 [CreateAssetMenu(fileName = "ItemPool")]
