aca1be34e19a32ee3877dd7f46920b7499cf08c1
Wafflgeasm <85540855+Wafflgeasm@users.noreply.github.com>
2022-10-16 13:39:18 -0500

    Progress made on item pool and upgrade system

--- ACCGameJamProject/Assets/Scripts/ScriptableObjects/ShopItemSO.cs
+++ ACCGameJamProject/Assets/Scripts/ScriptableObjects/ShopItemSO.cs
@@ -1,26 +1,15 @@
 using UnityEngine;
-using System;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-
-
-public class NamedArrayAttribute : PropertyAttribute
-{
-    public Type TargetEnum;
-    public NamedArrayAttribute(Type TargetEnum)
-    {
-        this.TargetEnum = TargetEnum;
-    }
-}
-public enum eUpgradeType {none, passive, weapon}
-public enum eItemPool { none, OldLady }
+public enum eUpgradeType {none, currentHealth, maxHealth, movementSpeed, suckSize, suckDuration, weapon}
 
 [CreateAssetMenu(fileName = "NewItem")]
 public class ShopItemSO : ScriptableObject
 {
+    public string ItemName;
+    public string ItemDescription;
     public eUpgradeType itemUpgradeType;
-    [NamedArray(typeof(eItemPool))] public bool[] itemPool;
     public Sprite itemSprite;
     public int itemCost;
 }
